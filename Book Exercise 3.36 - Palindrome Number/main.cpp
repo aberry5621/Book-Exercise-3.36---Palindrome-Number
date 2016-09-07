f43081cd4efbfa6d7f27f9e09c148910c2b27408
alexberry721 <alexberry721@gmail.com>
2016-09-07 10:18:38 -0700

    Init commit

--- Book Exercise 3.36 - Palindrome Number/main.cpp
+++ Book Exercise 3.36 - Palindrome Number/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Program Title
+    cout << "Palindrome Number ";
+    
+    // data storage
+    int input_number = 0;
+    int first_digit = 0;
+    int second_digit = 0;
+    int third_digit = 0;
+    
+    // I
+    cout << "Enter a three digit integer: ";
+    cin >> input_number;
+    
+    // P
+    // divide off digits by modulus and store
+    first_digit = (input_number % 10);
+    second_digit = ((input_number - first_digit) / 10) % 10;
+    third_digit = ((((input_number - first_digit) / 10) - second_digit) / 10) % 10;
+    
+    // O
+
+    if (first_digit == third_digit) {
+        cout << input_number << " is a palindrome number." << endl;
+    } else {
+        cout << input_number << " is not a palindrome number." << endl;
+    }
+    
     return 0;
 }
