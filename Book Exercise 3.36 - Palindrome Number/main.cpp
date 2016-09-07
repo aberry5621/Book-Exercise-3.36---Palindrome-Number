58ab1ead42c6995a37f90103b0157663237237d3
alexberry721 <alexberry721@gmail.com>
2016-09-07 10:10:42 -0700

    Initial Commit

---
+++ Book Exercise 3.36 - Palindrome Number/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 3.36 - Palindrome Number
+//
+//  Created by ax on 9/7/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
